f29ebd2e9ec429b80bbe595f998c7e71aa2585be
Miquel Llorens <miquel.llorens@uv.es>
2022-10-17 17:57:40 +0200

    Fix module name at import

--- dictionary_learning.py
+++ dictionary_learning.py
@@ -1,11 +1,11 @@
 """Try to import all available dictionaries."""
 
 try:
-    from _dictionary_spams import DictionarySpams
+    from ._dictionary_spams import DictionarySpams
 except ImportError:
     print("WARNING: Spams not installed, 'DictionarySpams' won't be available")
 
 # try:
-#     from _dictionary_sklearn import DictionarySklearn
+#     from ._dictionary_sklearn import DictionarySklearn
 # except ImportError:
 #     print("WARNING: Scikit-learn not installed, 'DictionarySklearn' won't be available")
